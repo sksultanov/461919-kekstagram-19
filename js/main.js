3bee9ed2f103926a82d3c66da33492472c380ef7
Шамиль <skaish@yandex.ru>
2020-02-11 22:53:54 +0500

    Что то сделал, доверяй но проверяй

--- js/main.js
+++ js/main.js
@@ -60,4 +60,95 @@ for (var j = 0; j < captionObject.length; j++) {
 }
 similarListElement.appendChild(fragment);
 
+var ESC_KEY = 'Escape';
+var pageBody = document.querySelector('body');
+var uploadPhotoForm = document.querySelector('.img-upload');
+var uploadFileInput = uploadPhotoForm.querySelector('#upload-file');
+
+var imgUploadOverlay = uploadPhotoForm.querySelector('.img-upload__overlay');
+var closeUploadFormButton = uploadPhotoForm.querySelector('#upload-cancel');
+
+var hashtagsInput = uploadPhotoForm.querySelector('.text__hashtags');
+var commentInput = uploadPhotoForm.querySelector('.text__description');
+
+var onСloseUploadEscPress = function (evt) {
+  if ((document.activeElement !== hashtagsInput) && (document.activeElement !== commentInput)) {
+    if (evt.key === ESC_KEY) {
+      closeUploadForm();
+    }
+  }
+};
+var openUploadForm = function () {
+  imgUploadOverlay.classList.remove('hidden');
+  pageBody.classList.add('modal-open');
+  document.addEventListener('keydown', onСloseUploadEscPress);
+};
+
+var closeUploadForm = function () {
+  imgUploadOverlay.classList.add('hidden');
+  pageBody.classList.remove('modal-open');
+  document.removeEventListener('keydown', onСloseUploadEscPress);
+  uploadFileInput.value = '';
+};
+
+uploadFileInput.addEventListener('change', function () {
+  openUploadForm();
+});
+
+closeUploadFormButton.addEventListener('click', function () {
+  closeUploadForm();
+});
+
+var LEVEL_MIN = 25;
+var LEVEL_DEFAULT = 100;
+var LEVEL_INC = 25;
+
+var previewPhoto = uploadPhotoForm.querySelector('.img-upload__preview');
+var levelButtonMinus = document.querySelector('.scale__control--smaller');
+var levelButtonPlus = document.querySelector('.scale__control--bigger');
+var levelValueInput = document.querySelector('.scale__control--value');
+
+var setLevelValue = function (levelValue) {
+  levelValueInput.value = levelValue;
+  previewPhoto.style.transform = 'scale(' + levelValueInput.value / LEVEL_DEFAULT + ')';
+};
+setLevelValue(LEVEL_DEFAULT);
+
+var increasePictureSize = function () {
+  levelValueInput.value = parseInt(levelValueInput.value, 10) + LEVEL_INC;
+  if (levelValueInput.value > LEVEL_DEFAULT) {
+    levelValueInput.value = LEVEL_DEFAULT;
+  }
+  setLevelValue(levelValueInput.value);
+};
+
+levelButtonPlus.addEventListener('click', function () {
+  increasePictureSize();
+});
+
+var decreasePictureSize = function () {
+  levelValueInput.value = parseInt(levelValueInput.value, 10) - LEVEL_INC;
+  if (levelValueInput.value < LEVEL_MIN) {
+    levelValueInput.value = LEVEL_MIN;
+  }
+  setLevelValue(levelValueInput.value);
+};
+
+levelButtonMinus.addEventListener('click', function () {
+  decreasePictureSize();
+});
+
+var effectLevelPin = imgUploadOverlay.querySelector('.effect-level__pin');
+var effectLevelValue = imgUploadOverlay.querySelector('.effect-level__value');
+var effectLevelDepth = imgUploadOverlay.querySelector('.effect-level__depth');
+
+effectLevelPin.addEventListener('mousedown', function () {
+  effectLevelPin.style.left = '100%';
+  effectLevelDepth.style.width = effectLevelPin.style.left;
+  effectLevelValue.value = effectLevelPin.style.left;
+});
+
+var imgEffects = imgUploadOverlay.querySelector('.img-upload__effects');
+
+var pinEnd = 455 + 'px';
 
